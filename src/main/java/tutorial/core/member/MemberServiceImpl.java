b975da15abd8db3c1428462cf57275cfae040254
smileyr909 <smileyr909@gmail.com>
2022-08-11 03:24:51 +0900

    MemberServiceTest commit

--- src/main/java/tutorial/core/member/MemberServiceImpl.java
+++ src/main/java/tutorial/core/member/MemberServiceImpl.java
@@ -5,11 +5,13 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
+    // 회원 가입
     @Override
     public void join(Member member) {
         memberRepository.save(member);
     }
 
+    // 회원 조회
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
---
+++ src/test/java/tutorial/core/member/MemberServiceTest.java
@@ -0,0 +1,23 @@
+package tutorial.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);    //주어진 member를 join한 것과 find로 찾은 member가 같은지 비교
+
+    }
+}
